476183f70fd02520748f439a20878a77c50bf7d1
Abinaya Goud <168286063+abinayagoudjandhyala@users.noreply.github.com>
2026-02-27 10:13:02 +0530

    Even or odd using Bitwise

---
+++ Number problems/EvenoroddusingBitwise.java
@@ -0,0 +1,10 @@
+public class EvenoroddusingBitwise {
+    public static void main(String[] args) {
+        int n =9;
+        if((n&1)==1){
+            System.out.println("Odd");
+        }else{
+            System.out.println("Even");
+        }
+    }
+}
