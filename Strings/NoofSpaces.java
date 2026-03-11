d0d3ac373245adc36b017b3293a0f973b6517c71
Abinaya Goud <168286063+abinayagoudjandhyala@users.noreply.github.com>
2026-03-11 10:05:50 +0530

    Number ofspaces

---
+++ Strings/NoofSpaces.java
@@ -0,0 +1,14 @@
+
+
+class Main {
+    public static void main(String[] args) {
+        String str="hello my name is abinayagoud";
+        int count =0;
+        for(int i = 0; i < str.length(); i++) {
+            if(str.charAt(i) == ' ') {
+                count++;
+            }
+        }
+        System.out.println(count);
+    }
+}
