11fa7538034814a90c468af9fb3747a82dff3b2d
Abinaya Goud <168286063+abinayagoudjandhyala@users.noreply.github.com>
2026-03-11 10:08:52 +0530

    Number of Digits.java

---
+++ Strings/NumberOfDigits.java
@@ -0,0 +1,14 @@
+
+class Main {
+    public static void main(String[] args) {
+        String str ="abc123def456";
+        int count =0;
+        for(int i=0;i<str.length();i++){
+            if(Character.isDigit(str.charAt(i))){
+                count++;
+            }
+        }
+        System.out.println(count);
+        
+    }
+}
